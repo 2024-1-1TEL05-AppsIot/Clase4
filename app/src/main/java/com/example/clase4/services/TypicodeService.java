d9aac0093d29ae9f17bcaff8c35a031bd7fc6e0e
oscar-diaz <oscarpucp@gmail.com>
2024-04-08 14:25:34 -0500

    Se añade un nuevo metodo a la interface "existePost" que recibe un entero como parámetro.

--- app/src/main/java/com/example/clase4/services/TypicodeService.java
+++ app/src/main/java/com/example/clase4/services/TypicodeService.java
@@ -2,10 +2,12 @@ package com.example.clase4.services;
 
 import com.example.clase4.dto.Comment;
 import com.example.clase4.dto.Profile;
+import com.example.clase4.dto.Post;
 
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Field;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
@@ -16,6 +18,9 @@ public interface TypicodeService {
 
     @GET("/typicode/demo/comments")
     Call<List<Comment>> getComments();
+
+    @GET("/typicode/demo/posts")
+    Call<List<Post>> existePost(@Query("id") int id);
 
     // de la forma /prueba?nombre=pedro&apellido=perez
     @GET("/prueba")
